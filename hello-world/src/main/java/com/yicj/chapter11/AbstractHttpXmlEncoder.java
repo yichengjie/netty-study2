896f6334c8b1f8073807890564a3432e3cca5b0e
易成杰 <626659321@qq.com>
2019-12-24 14:29:28 +0800

    xml + request

---
+++ hello-world/src/main/java/com/yicj/chapter11/AbstractHttpXmlEncoder.java
@@ -0,0 +1,43 @@
+package com.yicj.chapter11;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+import org.jibx.runtime.BindingDirectory;
+import org.jibx.runtime.IBindingFactory;
+import org.jibx.runtime.IMarshallingContext;
+import org.jibx.runtime.JiBXException;
+
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.util.List;
+
+public class AbstractHttpXmlEncoder<T> extends MessageToMessageEncoder<T> {
+    private IBindingFactory factory = null ;
+    private StringWriter writer = null ;
+    final static String CHARSET_NAME = "UTF-8" ;
+    final static Charset UTF_8 = Charset.forName(CHARSET_NAME) ;
+
+    protected ByteBuf encode0(ChannelHandlerContext ctx ,Object body) throws Exception {
+        factory = BindingDirectory.getFactory(body.getClass()) ;
+        writer = new StringWriter() ;
+        IMarshallingContext mctx = factory.createMarshallingContext() ;
+        mctx.setIndent(2);
+        mctx.marshalDocument(body,CHARSET_NAME,null,writer);
+        String xmlStr = writer.toString() ;
+        writer.close();
+        writer = null ;
+        ByteBuf encodBuf = Unpooled.copiedBuffer(xmlStr,UTF_8) ;
+        return encodBuf ;
+    }
+
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, T t, List<Object> list) throws Exception {
+        //释放资源
+        if(writer != null){
+            writer.close();
+            writer = null ;
+        }
+    }
+}
---
+++ hello-world/src/main/java/com/yicj/chapter11/HttpXmlRequest.java
@@ -0,0 +1,13 @@
+package com.yicj.chapter11;
+
+import io.netty.handler.codec.http.FullHttpRequest;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+public class HttpXmlRequest {
+    private FullHttpRequest request ;
+    private Object body ;
+}
