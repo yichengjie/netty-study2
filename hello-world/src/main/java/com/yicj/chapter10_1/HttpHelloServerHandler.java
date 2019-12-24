1eb01d8293c340ee76fa8f472137b168fb4597c6
易成杰 <626659321@qq.com>
2019-12-24 10:12:28 +0800

    保存

--- hello-world/src/main/java/com/yicj/chapter10_1/HttpHelloServerHandler.java
+++ hello-world/src/main/java/com/yicj/chapter10_1/HttpHelloServerHandler.java
@@ -4,11 +4,17 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.handler.codec.http.*;
+import io.netty.handler.stream.ChunkedFile;
 import io.netty.util.CharsetUtil;
 import javax.activation.MimetypesFileTypeMap;
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
 import static io.netty.handler.codec.http.HttpHeaderNames.LOCATION;
+import static io.netty.handler.codec.http.HttpHeaderUtil.isKeepAlive;
+import static io.netty.handler.codec.http.HttpHeaderUtil.setContentLength;
 
 
 public class HttpHelloServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
@@ -16,7 +22,8 @@ public class HttpHelloServerHandler extends SimpleChannelInboundHandler<FullHttp
     //消息接入方法
     @Override
     protected void messageReceived(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
-        this.sendError(ctx,HttpResponseStatus.METHOD_NOT_ALLOWED);
+        //this.sendError(ctx,HttpResponseStatus.METHOD_NOT_ALLOWED);
+        this.sendChunkfile(ctx,request);
     }
 
     @Override
@@ -24,6 +31,50 @@ public class HttpHelloServerHandler extends SimpleChannelInboundHandler<FullHttp
         cause.printStackTrace();
         if(ctx.channel().isActive()){
             sendError(ctx,HttpResponseStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void sendChunkfile(ChannelHandlerContext ctx, FullHttpRequest request) throws IOException {
+        File file = new File("D:\\tools\\jboss-5.1.0.GA.zip") ;
+        //以只读的方式打开文件，如果打开失败返回404错误
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
+        //获取文件的长度构造成功的HTTP应答消息
+        long fileLength = randomAccessFile.length();
+        HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        setContentLength(response, fileLength);
+        setContentTypeHeader(response, file);
+        //判断是否是keepAlive，如果是就在响应头中设置CONNECTION为keepAlive
+        if(isKeepAlive(request)){
+            response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+        }
+        ctx.write(response);
+
+        ChannelFuture sendFileFuture;
+        //通过Netty的ChunkedFile对象直接将文件写入到发送缓冲区中
+        sendFileFuture = ctx.write(new ChunkedFile(randomAccessFile,0,fileLength,8192),ctx.newProgressivePromise());
+        //为sendFileFuture添加监听器，如果发送完成打印发送完成的日志
+        sendFileFuture.addListener(new ChannelProgressiveFutureListener() {
+            @Override
+            public void operationComplete(ChannelProgressiveFuture future)
+                    throws Exception {
+                System.out.println("Transfer complete.");
+            }
+            @Override
+            public void operationProgressed(ChannelProgressiveFuture future, long progress, long total)
+                    throws Exception {
+                if(total<0){
+                    System.out.println("Transfer progress: "+progress);
+                }else {
+                    System.out.println("Transfer progress: "+progress+"/"+total);
+                }
+            }
+        });
+        //如果使用chunked编码，最后需要发送一个编码结束的空消息体，将LastHttpContent.EMPTY_LAST_CONTENT发送到缓冲区中，
+        //来标示所有的消息体已经发送完成，同时调用flush方法将发送缓冲区中的消息刷新到SocketChannel中发送
+        ChannelFuture lastContentFuture = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        //如果是非keepAlive的，最后一包消息发送完成后，服务端要主动断开连接
+        if(!isKeepAlive(request)){
+            lastContentFuture.addListener(ChannelFutureListener.CLOSE);
         }
     }
 
@@ -58,8 +109,8 @@ public class HttpHelloServerHandler extends SimpleChannelInboundHandler<FullHttp
         ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
     }
 
-    private void setContentTypeHeader(HttpResponse response,File file){
-        MimetypesFileTypeMap mimetypesTypeMap=new MimetypesFileTypeMap();
-        response.headers().set(CONTENT_TYPE,mimetypesTypeMap.getContentType(file.getPath()));
+    private void setContentTypeHeader(HttpResponse response, File file){
+        MimetypesFileTypeMap mimetypesTypeMap = new MimetypesFileTypeMap();
+        response.headers().set(CONTENT_TYPE, mimetypesTypeMap.getContentType(file.getPath()));
     }
 }
