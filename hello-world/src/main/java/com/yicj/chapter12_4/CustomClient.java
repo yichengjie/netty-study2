a1a6dad886692002e0de1400dcb71d9393495b74
易成杰 <626659321@qq.com>
2019-12-25 22:21:46 +0800

    sd

--- hello-world/src/main/java/com/yicj/chapter12_4/CustomClient.java
+++ hello-world/src/main/java/com/yicj/chapter12_4/CustomClient.java
@@ -5,26 +5,26 @@ import com.yicj.chapter12_4.handler.CustomClientHandler;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;  
-import io.netty.channel.socket.nio.NioSocketChannel;  
-  
-public class CustomClient {  
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class CustomClient {
 
     public void connect(int port, String host) throws InterruptedException {
         // Configure the client.
         EventLoopGroup group = new NioEventLoopGroup();
         try {
             Bootstrap b = new Bootstrap();
-            b.group(group)
-                    .channel(NioSocketChannel.class)
-                    .option(ChannelOption.TCP_NODELAY, true)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new CustomEncoder());
-                            ch.pipeline().addLast(new CustomClientHandler());
-                        }
-                    });
+            b.group(group);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.TCP_NODELAY, true);
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new CustomEncoder());
+                    ch.pipeline().addLast(new CustomClientHandler());
+                }
+            });
             ChannelFuture future = b.connect(host, port).sync();
             future.channel().writeAndFlush("Hello Netty Server ,I am a common client");
             future.channel().closeFuture().sync();
@@ -34,7 +34,7 @@ public class CustomClient {
     }
 
     public static void main(String[] args) throws Exception {
-        int port = 8080 ;
-        new CustomClient().connect(port,"127.0.0.1");
+        int port = 8080;
+        new CustomClient().connect(port, "127.0.0.1");
     }
 }
