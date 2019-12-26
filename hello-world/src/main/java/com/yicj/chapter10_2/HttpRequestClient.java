7727bd36815ac0422805c468a41dbbc20a2fab17
易成杰 <626659321@qq.com>
2019-12-26 20:52:01 +0800

    补充

--- hello-world/src/main/java/com/yicj/chapter10_2/HttpRequestClient.java
+++ hello-world/src/main/java/com/yicj/chapter10_2/HttpRequestClient.java
@@ -1,27 +1,23 @@
 package com.yicj.chapter10_2;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.*;
-import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.stream.ChunkedWriteHandler;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.UnsupportedEncodingException;
-import java.net.InetAddress;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.charset.Charset;
 
 @Slf4j
 public class HttpRequestClient {
 
     public static void main(String[] args) throws Exception {
-        String url = "http://localhost:9090/hello-boot/getUserInfo" ;
+        String url = "http://localhost:9090" ;
         URI uri = new URI(url) ;
         String host = uri.getHost();
         int port = uri.getPort();
@@ -44,27 +40,14 @@ public class HttpRequestClient {
                     p.addLast("http-aggregator",new HttpObjectAggregator(65536)) ;
                     p.addLast("http-decoder",new HttpRequestEncoder()) ;//出站
                     p.addLast("http-chunked",new ChunkedWriteHandler()) ;//出站
+                    p.addLast("http-decompress",new HttpContentDecompressor()) ;
                     p.addLast(new HttpRequestClientHandler()) ;//入站
-
                 }
             }) ;
-            String content = "hello post";
             //发起异步连接操作，并同步等待连接成功
             ChannelFuture f = b.connect(host, port).sync();
-            Channel channel = f.channel();
-            FullHttpRequest req = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST,
-                    "/hello-boot/getUserInfo", Unpooled.wrappedBuffer(content.getBytes("UTF-8")));
-
-            req.headers().set(HttpHeaderNames.HOST, "127.0.0.1");
-            req.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
-            int length = req.content().readableBytes();
-            req.headers().set(HttpHeaderNames.CONTENT_LENGTH,length+"");
-            req.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/json");
-            String str = req.content().toString(0, req.content().capacity(), Charset.defaultCharset());
-            log.info("str : {}",str);
-            channel.writeAndFlush(req).sync();
             //同步等待客户端链路关闭
-            channel.closeFuture().sync() ;
+            f.channel().closeFuture().sync() ;
         }finally {
             //优雅退出，释放NIO线程组
             group.shutdownGracefully();
@@ -73,8 +56,32 @@ public class HttpRequestClient {
 
     class HttpRequestClientHandler extends ChannelHandlerAdapter{
         @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            URI uri = new URI("/hello-boot/getUserInfo") ;
+            String content = "hello, world";
+            FullHttpRequest req = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST,
+                    uri.toASCIIString(), Unpooled.copiedBuffer(content.getBytes("UTF-8")));
+            req.headers().set(HttpHeaderNames.HOST, "127.0.0.1");
+            HttpHeaderUtil.setKeepAlive(req,false);
+            //req.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+            int length = req.content().readableBytes();
+            req.headers().set(HttpHeaderNames.CONTENT_LENGTH,length+"");
+            req.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/json");
+            log.info("req info : {}", req);
+            //String requestContent = req.content().toString(0, req.content().capacity(), Charset.defaultCharset());
+            //log.info("requestContent : {}",requestContent);
+            ctx.writeAndFlush(req).sync();
+        }
+
+        @Override
         public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-            log.info("msg : {}" ,msg);
+            FullHttpResponse response = (FullHttpResponse) msg ;
+            log.info("response info : {}" , response);
+            ByteBuf content = response.content();
+            byte [] bytes = new byte[content.readableBytes()] ;
+            content.readBytes(bytes) ;
+            String respContent = new String(bytes,"UTF-8") ;
+            log.info("respContent : {}" ,respContent);
         }
     }
 }
