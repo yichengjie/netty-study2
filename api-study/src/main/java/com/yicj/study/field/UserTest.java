9f8bb291e1516226f69841c8741990101b3ef7c9
易成杰 <626659321@qq.com>
2020-01-08 20:37:01 +0800

    保存

--- api-study/src/main/java/com/yicj/study/field/UserTest.java
+++ api-study/src/main/java/com/yicj/study/field/UserTest.java
@@ -1,19 +1,12 @@
 package com.yicj.study.field;
 
-import sun.misc.Unsafe;
-
 import java.lang.reflect.Field;
 
 public class UserTest {
     public static void main(String[] args) throws Exception {
-     /*   Field field = User.class.getDeclaredField("username");
+        Field field = User.class.getDeclaredField("username");
         field.setAccessible(true);
         Object o = field.get(null);
-        System.out.println(o);*/
-        /*User user = new User() ;
-        Field field = User.class.getDeclaredField("num");
-        field.setAccessible(true);
-        field.set(user,"abc");
-        System.out.println(field.get(user));*/
+        System.out.println(o);
     }
 }
--- api-study/src/main/java/com/yicj/study/unsafe/Main.java
+++ api-study/src/main/java/com/yicj/study/unsafe/Main.java
@@ -10,7 +10,6 @@ public class Main {
     private long s2 ;
 
     public static void main(String[] args) throws Exception{
-
         Main main = new Main() ;
         main.s1 = 20 ;
         long s ,next ;
@@ -22,23 +21,24 @@ public class Main {
         long except = 1 ;
         long real = 1 ;
         Unsafe unsafe = null ;
-        Field theUnsafeInstance = null ;
-        theUnsafeInstance = Unsafe.class.getDeclaredField("theUnsafe") ;
+        Field theUnsafeInstance = Unsafe.class.getDeclaredField("theUnsafe") ;
         theUnsafeInstance.setAccessible(true);
         unsafe = (Unsafe) theUnsafeInstance.get(null) ;
         System.out.println("====================");
         Field[] fields = Main.class.getDeclaredFields();
-        for (Field field : fields){
-            System.out.println(field.getName() +"====> " +unsafe.objectFieldOffset(field));
-        }
+
+        long s2offset = unsafe.objectFieldOffset(Main.class.getDeclaredField("s2"));
+        System.out.println("s2offset :" + s2offset  );
+        /*for (Field field : fields){
+            System.out.println(field.getName() +"====> " + unsafe.objectFieldOffset(field));
+        }*/
         /**
          * s====> 24
          * s1====> 8
          * s2====> 16
          */
         System.out.println("====================");
-        boolean flag = unsafe.compareAndSwapLong(main, 16, 0, 21);
-        System.out.println(flag);
+        boolean flag = unsafe.compareAndSwapLong(main, s2offset, 0, 21);
         System.out.println(main.s2);
     }
 }
